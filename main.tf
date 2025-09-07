1dae6cd5b3b102ff38abf0082f56d7880fa45aaa
Adil Ahmed <adil@adils-mbp.mynetworksettings.com>
2025-09-07 10:27:25 -0400

    uploading tf code draft 1 attempt 5

---
+++ main.tf
@@ -0,0 +1,99 @@
+resource "google_compute_network" "vpc1" {
+  name                    = "vpc1"
+  auto_create_subnetworks = false
+}
+
+resource "google_compute_subnetwork" "subnet1" {
+  name          = "subnet1"
+  region        = var.region
+  network       = google_compute_network.vpc1.id
+  ip_cidr_range = var.subnet1_cidr
+
+  secondary_ip_range {
+    range_name    = "pods1"
+    ip_cidr_range = var.pods_cidr
+  }
+  secondary_ip_range {
+    range_name    = "services1"
+    ip_cidr_range = var.services_cidr
+  }
+}
+
+resource "google_compute_subnetwork" "subnet2" {
+  name          = "subnet2"
+  region        = var.region
+  network       = google_compute_network.vpc1.id
+  ip_cidr_range = var.subnet2_cidr
+}
+
+resource "google_project_iam_member" "clusterrole1" {
+  project = var.project_id
+  role    = "roles/container.admin"
+  member  = "user:${var.admin_user_email}"
+}
+
+resource "google_service_account" "nodesa1" {
+  account_id   = "nodesa1"
+  display_name = "nodesa1"
+}
+
+resource "google_project_iam_member" "noderole_service_account" {
+  project = var.project_id
+  role    = "roles/container.nodeServiceAccount"
+  member  = "serviceAccount:${google_service_account.nodesa1.email}"
+}
+
+resource "google_project_iam_member" "noderole_logging" {
+  project = var.project_id
+  role    = "roles/logging.logWriter"
+  member  = "serviceAccount:${google_service_account.nodesa1.email}"
+}
+
+resource "google_project_iam_member" "noderole_monitor" {
+  project = var.project_id
+  role    = "roles/monitoring.metricWriter"
+  member  = "serviceAccount:${google_service_account.nodesa1.email}"
+}
+
+resource "google_container_cluster" "cluster1" {
+  name       = var.cluster_name
+  location   = var.zone
+  network    = google_compute_network.vpc1.id
+  subnetwork = google_compute_subnetwork.subnet1.name
+
+  ip_allocation_policy {
+    cluster_secondary_range_name  = "pods1"
+    services_secondary_range_name = "services1"
+  }
+
+  remove_default_node_pool = true
+  initial_node_count       = 1
+}
+
+resource "google_container_node_pool" "nodepool1" {
+  name     = "nodepool1"
+  cluster  = google_container_cluster.cluster1.name
+  location = var.zone
+
+  node_count = var.node_count
+
+  node_config {
+    machine_type    = var.node_machine_type
+    service_account = google_service_account.nodesa1.email
+    oauth_scopes = [
+      "https://www.googleapis.com/auth/devstorage.read_only",
+      "https://www.googleapis.com/auth/logging.write",
+      "https://www.googleapis.com/auth/monitoring",
+      "https://www.googleapis.com/auth/trace.append"
+    ]
+  }
+
+  depends_on = [
+    google_container_cluster.cluster1,
+    google_project_iam_member.noderole_service_account
+  ]
+}
+
+output "cluster_endpoint" {
+  value = google_container_cluster.cluster1.endpoint
+}
---
+++ variables.tf
@@ -0,0 +1,57 @@
+variable "project_id" {
+  type = string
+}
+
+variable "region" {
+  type    = string
+  default = "us-central1"
+}
+
+variable "zone" {
+  type    = string
+  default = "us-central1-a"
+}
+
+variable "backend_bucket" {
+  type = string
+}
+
+variable "cluster_name" {
+  type    = string
+  default = "gke-cluster1"
+}
+
+variable "subnet1_cidr" {
+  type    = string
+  default = "10.10.0.0/24"
+}
+
+variable "subnet2_cidr" {
+  type    = string
+  default = "10.10.1.0/24"
+}
+
+variable "pods_cidr" {
+  type    = string
+  default = "10.20.0.0/14"
+}
+
+variable "services_cidr" {
+  type    = string
+  default = "10.24.0.0/20"
+}
+
+variable "node_machine_type" {
+  type    = string
+  default = "e2-micro"
+}
+
+variable "node_count" {
+  type    = number
+  default = 2
+}
+
+# gke admin
+variable "admin_user_email" {
+  type = string
+}
